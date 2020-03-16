8966dac735751500a64c9b6f21ba62b57abe06fa
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-03-16 21:20:07 +0700

    membuat mapping router

---
+++ app/url_mappings.go
@@ -0,0 +1,10 @@
+package app
+
+import "bookstore/controllers"
+
+func mapUrls() {
+	router.GET("/ping", controllers.Ping)
+
+	router.POST("/users", controllers.CreateUser)
+	router.GET("/users/:user_id", controllers.GetUser)
+}
