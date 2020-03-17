87dbce733e65ae3f47361dc81d30db22e5ec0476
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-03-17 18:59:07 +0700

    modifikasi lanjut controller untuk create user

--- controllers/users_controllers.go
+++ controllers/users_controllers.go
@@ -1,12 +1,44 @@
 package controllers
 
 import (
+	"bookstore/domain/users"
+	"bookstore/services"
+	"bookstore/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusCreated, "Created new user")
+	err := c.Request.ParseForm()
+	val := c.Request.Form
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, errors.RestErrors{
+			Message: err.Error(),
+			Code:    http.StatusBadRequest,
+			Error:   "bad_request",
+		})
+	}
+
+	user := users.User{
+		Id:          1,
+		FirstName:   val.Get("first_name"),
+		LastName:    val.Get("last_name"),
+		Email:       val.Get("email"),
+		DateCreated: val.Get("date_created"),
+	}
+
+	result, Err := services.CreateUser(user)
+
+	if Err != nil {
+		c.JSON(http.StatusBadRequest, errors.RestErrors{
+			Message: Err.Error(),
+			Code:    http.StatusBadRequest,
+			Error:   "bad_request",
+		})
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
