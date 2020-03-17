69c71d5b80bd0867244ccb83a08010104ac7de67
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-03-17 22:28:29 +0700

    modifikasi lanjut fungsi servis terkait validasi input

--- services/users_service.go
+++ services/users_service.go
@@ -1,7 +1,28 @@
 package services
 
-import "bookstore/domain/users"
+import (
+	"bookstore/domain/users"
+	"bookstore/utils/errors"
+	"github.com/go-playground/validator/v10"
+)
 
-func CreateUser(user users.User) (*users.User, error) {
+var (
+	validate = validator.New()
+)
+
+func CreateUser(user users.User) (*users.User, []errors.ValidationErrors) {
+	var messages []errors.ValidationErrors
+
+	if err := validate.Struct(&user); err != nil {
+		for _, e := range err.(validator.ValidationErrors) {
+			messages = append(messages, errors.ValidationErrors{
+				Field: e.Field(),
+				Tag:   e.Tag(),
+			})
+		}
+
+		return nil, messages
+	}
+
 	return &user, nil
 }
