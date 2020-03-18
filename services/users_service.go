347e1bfd2dfef651cc8ef0899ae76410a5f86a45
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-03-18 09:51:55 +0700

    menambahkan fungsi servis get user

--- services/users_service.go
+++ services/users_service.go
@@ -3,26 +3,31 @@ package services
 import (
 	"bookstore/domain/users"
 	"bookstore/utils/errors"
-	"github.com/go-playground/validator/v10"
+	"net/http"
 )
 
-var (
-	validate = validator.New()
-)
-
-func CreateUser(user users.User) (*users.User, []errors.ValidationErrors) {
-	var messages []errors.ValidationErrors
-
-	if err := validate.Struct(&user); err != nil {
-		for _, e := range err.(validator.ValidationErrors) {
-			messages = append(messages, errors.ValidationErrors{
-				Field: e.Field(),
-				Tag:   e.Tag(),
-			})
+func CreateUser(user users.User) (*users.User, *errors.RestErrors) {
+	if err := user.Validate(); len(err) > 0 {
+		return nil, &errors.RestErrors{
+			Message: err,
+			Code:    http.StatusBadRequest,
+			Error:   "bad_request",
 		}
+	}
 
-		return nil, messages
+	if err := user.Save(); err != nil {
+		return nil, err
 	}
 
 	return &user, nil
+}
+
+func GetUser(userId string) (*users.User, *errors.RestErrors) {
+	result := users.User{Id: userId}
+
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
 }
