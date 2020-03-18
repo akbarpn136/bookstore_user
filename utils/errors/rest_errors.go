bdb501467eff8bc3a9f361a191b6a915e175459f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-03-18 15:11:57 +0700

    membuat beberapa fungsi messages error

--- utils/errors/rest_errors.go
+++ utils/errors/rest_errors.go
@@ -1,7 +1,33 @@
 package errors
 
+import "net/http"
+
 type RestErrors struct {
 	Message []string `json:"message"`
 	Code    int      `json:"code"`
 	Error   string   `json:"error"`
+}
+
+func BadRequest(message []string) *RestErrors {
+	return &RestErrors{
+		Message: message,
+		Code:    http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NotFound(message []string) *RestErrors {
+	return &RestErrors{
+		Message: message,
+		Code:    http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NotAcceptable(message []string) *RestErrors {
+	return &RestErrors{
+		Message: message,
+		Code:    http.StatusNotAcceptable,
+		Error:   "not_acceptable",
+	}
 }
