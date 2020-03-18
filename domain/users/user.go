a7f9dff533fbbc657eb0fcbae1392d3641678182
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-03-18 09:44:58 +0700

    ganti nama file untuk user

--- domain/users/user.go
+++
@@ -1,9 +0,0 @@
-package users
-
-type User struct {
-	Id          int64  `json:"id"`
-	FirstName   string `json:"first_name" validate:"required"`
-	LastName    string `json:"last_name" validate:"required"`
-	Email       string `json:"email" validate:"required,email"`
-	DateCreated string `json:"date_created"`
-}
